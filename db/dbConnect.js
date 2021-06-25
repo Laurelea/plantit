ba5534470336c904b8bd5cc45fd070bf9c25e1d7
Laurelea <43186083+Laurelea@users.noreply.github.com>
2021-06-25 13:28:46 +0500

    Update dbConnect.js
    ('pg').Pool

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,11 +1,13 @@
-const {Client} = require('pg')
-const pg = new Client({
+const Pool = require('pg').Pool
+const db = new Pool({
     user: 'gsoxtcmgsqbxbi',
     host: 'ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
     database: 'dbfo3d84h3ca49',
     password: '42bd9db46f114e1424c2180635a13daf02b6e08cc8383dcf2ba8973748870b85',
     port: 5432,
     ssl: {
-    rejectUnauthorized: false
-  }
+        rejectUnauthorized: false
+    }
 })
+
+module.exports = {db}
