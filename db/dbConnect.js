7c5212b55e1bfffb311f73e6de6e669bd00fe6c3
Laurelea <43186083+Laurelea@users.noreply.github.com>
2021-06-25 12:05:50 +0500

    Update dbConnect.js
    rejectUnauthorized: false added

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -5,6 +5,7 @@ const pg = new Client({
     database: 'dbfo3d84h3ca49',
     password: '42bd9db46f114e1424c2180635a13daf02b6e08cc8383dcf2ba8973748870b85',
     port: 5432,
+    ssl: {
+    rejectUnauthorized: false
+  }
 })
-
-module.exports = pg
