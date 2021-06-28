18a46b51588c0587f130ebcf058b8f7b8e2823c3
Laurelea <43186083+Laurelea@users.noreply.github.com>
2021-06-28 15:19:33 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ const path = require("path");
 // const expressLoger = expressPino({logger})
 //End Logs
 
-app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'http')
-            res.redirect(`http://${req.header('host')}${req.url}`)
-        else
-            next()
-    }
-)
+// app.use((req, res, next) => {
+//         if (req.header('x-forwarded-proto') !== 'http')
+//             res.redirect(`http://${req.header('host')}${req.url}`)
+//         else
+//             next()
+//     }
+// )
 
 app.use(express.static(path.join(__dirname, '../client')));
 
