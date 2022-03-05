cccc8723e62bb681413297017207efd7fe6611e2
laurelea <laurelea@mail.ru>
2022-03-05 13:01:05 +0500

    table ok. pagination: to do

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import App from './App_grid';
+import App from './App.';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore, compose } from "redux";
